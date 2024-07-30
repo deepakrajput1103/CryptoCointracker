f9d82633b338018840046f75992dd064cfd1ad17
Deepak Singh <ds5947012@gmail.com>
2024-07-31 01:01:42 +0530

    add all files

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'; 
+import CoinContextProvider from './context/coinContext.jsx';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <CoinContextProvider>
+  <App/>
+  </CoinContextProvider>
+  </BrowserRouter>
+)
